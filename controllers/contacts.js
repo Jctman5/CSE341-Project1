1c91663db756937bf063098ee11ef0d47ae74acd
Jctman5 <Jctman5@gmail.com>
2026-09-19 18:18:48 -0400

    matchedCount

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -43,12 +43,10 @@ const updateContact = async (req, res) => {
         birthday: req.body.birthday
     };
     const response = await mongodb.getDatabase().db().collection("contacts").replaceOne({_id: contactId}, contact);
-    if (response.acknowledged){
-        res.status(204).send();
-    } 
-    else{
-        res.status(500).json(response.error || "Some error occured while updating the contact.");
+    if (response.matchedCount === 0) {
+        return res.status(404).json({ error: "Contact not found" });
     }
+    res.status(204).send();
 };
 const deleteContact = async (req, res) => {
     const contactId = new ObjectId(req.params.id);
